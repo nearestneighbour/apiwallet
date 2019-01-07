9f2f1336b37600fbabbb5994df8e0bbabee8d73b
nearestneighbour <tido_berg@hotmail.com>
2019-01-07 21:13:35 +0100

    removed deprecated code

--- wallet.py
+++ wallet.py
@@ -1,9 +1,8 @@
 # This class does ...
 
-from tabulate import tabulate
 import requests
 import json
-import pickle
+#import pickle --- see save/load functions
 
 from updatable import Updatable
 
@@ -65,67 +64,13 @@ class Wallet:
                     balance[b] += bal[b]
         return balance
 
+    """ include or not???
     def save(self, fname):
         # Save wallet object to file using pickle module
         # Does this store class functions as well? (i.e. does it not update with the code)
         with open(fname, 'wb') as f:
             pickle.dump(self, f)
-
-    def __repr__(self):
-        # Print wallet object
-        return 'Wallet({})'.format(len(self.accounts))
-
-    # DEPRECATED
-    def show(self, what):
-        return
-        # Print overview of accounts in wallet. Print total BTC per account,
-        # amount per currency per account, or amount per currency
-        total = self.total_btc # Update wallet
-        if 'total' in what:
-            eur = self.get_btceur
-            usd = self.get_btcusd
-            tot = 100/total
-            table = []
-            for acc in self.accounts:
-                table.append([acc.meta['name'],acc.btc,acc.btc*eur,acc.btc*usd,acc.btc*tot])
-            table.append(['Total value',self.btc,self.btc*eur,self.btc*usd,100])
-            print('\n',tabulate(table,headers=['Account name','Total BTC',
-            'Total EUR','Total USD','Percent']),'\n')
-        if 'balance' in what:
-            table = []
-            for acc in self.accounts:
-                j = True
-                balances = acc.get_balances()
-                values = acc.get_values()
-                for curr in balances:
-                    if j == True:
-                        table.append([acc.meta['name'],curr,balances[curr],values[curr]])
-                        j = False
-                    else:
-                        table.append(['',curr,balances[curr],values[curr]])
-            print('\n',tabulate(table, headers=['Account','Currency','Balance','BTC']),'\n')
-        if 'currency' in what:
-            bal = {}
-            pr = {}
-            for acc in self.accounts:
-                balances = acc.get_balances()
-                values = acc.get_values()
-                for curr in balances:
-                    if curr not in bal:
-                        val[curr] = values[curr]
-                        bal[curr] = balances[curr]
-                    else:
-                        val[curr] += values[curr]
-                        bal[curr] += balances[curr]
-            table = []
-            for curr in bal:
-                table.append([curr,bal[curr],val[curr],100*val[curr]/total])
-            print('\n',tabulate(table, headers=['Currency','Balance','BTC','Percent']),'\n')
-
-def load(fname):
-    # Load wallet object from file using pickle module
-    with open(fname, 'rb') as f:
-        return pickle.load(f)
+    """
 
 def load_btceur():
     # Get BTC/EUR price from Kraken
@@ -136,3 +81,10 @@ def load_btcusd():
     # Get BTC/USD price from Kraken
     url = 'https://api.kraken.com/0/public/Ticker?pair=xbtusd'
     return float(requests.get(url).json()['result']['XXBTZUSD']['c'][0])
+
+""" include or not??
+def load(fname):
+    # Load wallet object from file using pickle module
+    with open(fname, 'rb') as f:
+        return pickle.load(f)
+"""
