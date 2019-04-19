02deb21a2d2fe834034749538afa66cbac509b49
nearestneighbour <tido_berg@hotmail.com>
2019-04-19 14:16:54 +0200

    update

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,7 @@
 from .wallet import Wallet
-from .updatable import Updatable
-from .account import Account
 
 from .accounts.btc_xpub import btc_xpub
+from .accounts.btc_xpub import btc_address
 from .accounts.bitmex_account import bitmex_account
 from .accounts.kraken_account import kraken_account
 from .accounts.eos_account import eos_account
