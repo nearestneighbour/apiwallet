5a5c3bce1fa8a41252b296ad1dc0dcc4dfa27f0f
nearestneighbour <tido_berg@hotmail.com>
2019-04-19 15:18:10 +0200

    forgot line

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from .updatable import Updatable
 from .account import Account
 
 from .accounts.btc_xpub import btc_xpub
+from .accounts.btc_address import btc_address
 from .accounts.bitmex_account import bitmex_account
 from .accounts.kraken_account import kraken_account
 from .accounts.eos_account import eos_account
